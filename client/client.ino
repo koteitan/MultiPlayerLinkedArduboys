7581c160fbf3cf4469ccd2f94c29d588779cf856
koteitan <ukaibutton@mac.com>
2016-11-01 01:50:45 +0900

    client is implemented in 15 fps.

--- client/client.ino
+++ client/client.ino
@@ -1,13 +1,69 @@
-#include "Arduboy.h"
+#include <Arduboy.h>
+Arduboy arduboy;
+#define PLAYER_OWN (0)
+#define PLAYER_OPP (1)
+#define PLAYERS    (2)
+char keymapMP[PLAYERS]={0x00, 0x00};
 void setup() {
-  arduboy.begin();
+  arduboy.beginNoLogo();
   arduboy.setFrameRate(15);
+  Serial.setTimeout(100);
+  Serial.begin(9600);
 }
 
 void loop() {
+  //PIN -> latch own key -> cMPKey
+  for(int k=2;k<8;k++){
+    keymapMP[PLAYER_OWN] |= arduboy.pressed(_BV(k))?0x00:_BV(k);
+  }
+  // in active frame
   if (!(arduboy.nextFrame())) return;
+  //Serial -> receive opp key -> cMPKey
+#if 0
+  Serial.print((uint8_t)(keymapMP[PLAYER_OWN])>>2);
+  Serial.print(" ");
+#endif
+  Serial.write(&keymapMP[PLAYER_OWN],1);
+  Serial.readBytes(&keymapMP[PLAYER_OPP],1);
+
+  //clear for next
+  procAll();
+  keymapMP[PLAYER_OWN]=0x00;
+}
+void procAll(){
+  drawAll();
+}
+void drawAll(){
   arduboy.clear();
-  arduboy.setCursor(4, 9);
-  arduboy.print(F("Hello, world!"));
+  arduboy.setCursor( 0, 0);
+  arduboy.println("my key:");
+  arduboy.setCursor( 8, 8);
+  arduboy.println((keymapMP[PLAYER_OWN]&UP_BUTTON)?"":"U");
+  arduboy.setCursor( 0,16);
+  arduboy.println((keymapMP[PLAYER_OWN]&LEFT_BUTTON)?"":"L");
+  arduboy.setCursor(16,16);
+  arduboy.println((keymapMP[PLAYER_OWN]&RIGHT_BUTTON)?"":"R");
+  arduboy.setCursor(32,16);
+  arduboy.println((keymapMP[PLAYER_OWN]&A_BUTTON)?"":"A");
+  arduboy.setCursor(40,16);
+  arduboy.println((keymapMP[PLAYER_OWN]&B_BUTTON)?"":"B");
+  arduboy.setCursor( 8,24);
+  arduboy.println((keymapMP[PLAYER_OWN]&DOWN_BUTTON)?"":"D");
+
+  arduboy.setCursor( 0,32);
+  arduboy.println("your key:");
+  arduboy.setCursor( 8,40);
+  arduboy.println((keymapMP[PLAYER_OPP]&UP_BUTTON)?"":"U");
+  arduboy.setCursor( 0,48);
+  arduboy.println((keymapMP[PLAYER_OPP]&LEFT_BUTTON)?"":"L");
+  arduboy.setCursor(16,48);
+  arduboy.println((keymapMP[PLAYER_OPP]&RIGHT_BUTTON)?"":"R");
+  arduboy.setCursor(32,48);
+  arduboy.println((keymapMP[PLAYER_OPP]&A_BUTTON)?"":"A");
+  arduboy.setCursor(40,48);
+  arduboy.println((keymapMP[PLAYER_OPP]&B_BUTTON)?"":"B");
+  arduboy.setCursor( 8,56);
+  arduboy.println((keymapMP[PLAYER_OPP]&DOWN_BUTTON)?"":"D");
+
   arduboy.display();
 }
